643fd195b0dc5ba75a0d8cb9b6930324526f31f7
kkom3 <k.kenji681@gmail.com>
2021-05-13 13:24:15 +0000

    Update

---
+++ script.js
@@ -0,0 +1,37 @@
+longitude: 139.54879508947934, latitude: 35.723777284047515 ces = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+      return [
+                {
+                              name: 'Magnemite',
+                              location: {
+                                                lat: 44.496470,
+                                                lng: 11.320180,
+                                            }
+                          },
+            ];
+}
+
+function renderPlaces(places) {
+      let scene = document.querySelector('a-scene');
+
+      places.forEach((place) => {
+                let latitude = place.location.lat;
+                let longitude = place.location.lng;
+
+                let model = document.createElement('a-entity');
+                model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+                model.setAttribute('rotation', '0 180 0');
+                model.setAttribute('animation-mixer', '');
+                model.setAttribute('scale', '0.5 0.5 0.5');
+
+                model.addEventListener('loaded', () => {
+                              window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                          });
+
+                scene.appendChild(model);
+            });
+}
